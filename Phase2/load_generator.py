4fa91b4f912552039ceda532087c0af3353c522b
zed41 <amkewlchamp@gmail.com>
2017-11-03 21:00:18 +0530

    phase 2

--- Phase2/load_generator.py
+++ Phase2/load_generator.py
@@ -1,50 +1,135 @@
-import requests
-from time import time
 import threading
-import _thread
+import time
+import sys
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import subprocess
+import os
+import signal
 
-requests_count=0
-time_count=0
 
-lamda=200
-time_between_reqs=1/lamda
-test_start_time=time()
+all_started=threading.Event()
 
-lock = threading.Lock()
-response_count=0
-response_time=0
-def req(t):
+class user_obj (threading.Thread):
+    def __init__(self, tid):
+      threading.Thread.__init__(self)
+      self.tid=tid
+      self.resp_time=0
+      self.req_count=0
+      self.success_count=0
+
+    def run(self):
+      all_started.wait()
+      while(testing):
+          t_prev=time.time()
+          flag,t=req()
+          if flag:
+            self.resp_time+=t-t_prev
+            self.req_count+=1
+            self.success_count+=1
+          else:
+            self.resp_time=t-t_prev
+            self.req_count+=1
+          time.sleep(.1)
+
+
+
+def req():
     try:
-        r = requests.get('http://10.15.28.175:8080')
-        global response_time
-        global response_count
-        if(r.status_code==200):
-            lock.acquire()
-            response_time=(response_time*response_count+time()-t)/(response_count+1)
-            response_count+=1
-            lock.release()
-
+        r = requests.get('http://localhost:8080/',timeout=10.0)
+        return True,time.time()
     except:
-        return
+        return False,time.time()
 
 
-diff=0
-start_time=time()
-prev_req_count=1
-while(True):
-    if(requests_count/100>prev_req_count):
-        print('Request Count : ',requests_count,' Time Elapsed : ',time()-test_start_time,' Lamda : ',requests_count/(time()-test_start_time),\
-        ' Response Count : ',response_count,' Avg Response Time : ',response_time)
-        prev_req_count=int(requests_count/100)+1
-    time_diff=(time()-start_time)/time_between_reqs
-    if(int(time_diff)>1.):
+testing =True
 
-        diff+=(time_diff-int(time_diff))
-        for i in range(int(time_diff)):
-            requests_count+=1
-            _thread.start_new_thread(req,(time(),))
-        start_time=time()
-    if(diff>1.0):
-        requests_count+=1
-        _thread.start_new_thread(req,(time(),))
-        diff=diff-1
+N_min=int(sys.argv[1])
+N_max=int(sys.argv[2])
+step=int(sys.argv[3])
+test_time=int(sys.argv[4])
+
+print('Closed Loop Load Gen')
+print('====================')
+print('Config\n-------')
+print('N_min',N_min)
+print('N_max',N_max)
+print('Step',step)
+print('Test Time',test_time,'Secs')
+test_data=[]
+
+def test(N):
+    threads=[None]*N
+    global testing
+    testing=False
+    for i in range(N):
+        threads[i]=user_obj(i)
+    print('Thread Created')
+    print('--'*30)
+
+    for i in range(N):
+        threads[i].start()
+
+    all_started.set()
+    testing=True
+    print('Starting Test for N :',N)
+    time.sleep(test_time)
+    print('Test Completed')
+    testing=False
+    total_time=0
+    total_req_count=0
+    total_success_count=0
+    for i in range(N):
+        threads[i].join()
+        total_time+=threads[i].resp_time
+        total_req_count+=threads[i].req_count
+        total_success_count+=threads[i].success_count
+
+    td={}
+    td['N']=N
+    td['total_time']=total_time
+    td['total_req_count']=total_req_count
+    td['avg_resp_time']=total_time/total_req_count
+    td['success_count']=total_success_count
+    td['tp']=total_success_count/test_time
+    print('Test Details')
+    for x in td:
+        print(x,td[x])
+    print('--'*30)
+    print()
+    all_started.clear()
+    return td
+
+for N in range(N_min,N_max+1,step):
+    pro = subprocess.Popen('./server.out', stdout=subprocess.PIPE,shell=True, preexec_fn=os.setsid)
+    print('Started Server Pid :',os.getpgid(pro.pid))
+    td=test(N)
+    test_data.append(td)
+    os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
+    time.sleep(2)
+
+fig,ax=plt.subplots()
+df_td=pd.DataFrame(test_data)
+# df_td=pd.read_csv('results.csv')
+df_td['throughtput']=df_td['tp']
+df_td.to_csv("results.csv",index=False)
+ax.plot(df_td['N'],df_td['avg_resp_time'])
+ax.scatter(df_td['N'],df_td['avg_resp_time'],color='red')
+#ax.scatter(df_td['N'],df_td['avg_resp_time'],color='red')
+ax.set_ylabel('Turn around Time')
+ax.set_title('TAT vs Number of users')
+ax.set_xlabel('Nmmber of users')
+fig.savefig('tat.png')
+plt.close(fig);
+print("here ")
+fig,ax=plt.subplots()
+ax.plot(df_td['N'],df_td['throughtput'])
+ax.scatter(df_td['N'],df_td['throughtput'],color='red')
+ax.set_ylabel('Throughput')
+ax.set_title('Throughput vs Number of users')
+ax.set_xlabel('Number of users')
+fig.savefig('throughput.png')
+
+plt.close(fig)
+#print the throughtput
---
+++ Phase2/readme_lr.txt
@@ -0,0 +1,11 @@
+Instruction to Run the Code
+===========================
+
+python3 gengen.py {startN} {endN} {step_size} {test_time}
+startN is the value of N to start the test
+endN is the value of N upto which to run the test
+step_size is the value of N to be changed after each test
+test_time is the time to run for each test in seconds
+
+The code automatically starts the server from a.out and runs
+it on port 8080
